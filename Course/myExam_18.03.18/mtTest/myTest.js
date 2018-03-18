a227c024f8c378103ca7362c8287dcebfe47b8a3
Milko Yanakiev <myanakiev@hotmail.com>
2018-03-18 17:03:18 +0200

    02

--- Course/myExam_18.03.18/mtTest/myTest.js
+++ Course/myExam_18.03.18/mtTest/myTest.js
@@ -1,14 +1,92 @@
 let expect = require("chai").expect;
-let tst = require("../fnctTst").tst;
-
-describe("Function createList tests", function () {
+let PaymentPackage = require("../02. Payment Package");
 
 
+describe("tests", function () {
+
+    let bill;
+    beforeEach(function () { // изчиства данните
+        bill = new PaymentPackage("Pesho", 20);
+    });
+
+    it("to str", function () {
+
+        let str = "Package: Pesho\n" +
+            "- Value (excl. VAT): 20\n" +
+            "- Value (VAT 20%): 24";
+        expect(bill.toString()).to.be.equal(str);
+    });
+
+    it("get name", function () {
+        let str = "Pesho";
+        expect(bill.name).to.be.equal(str);
+    });
+
+    it("get value", function () {
+        let val = 20;
+        expect(bill.value).to.be.equal(val);
+    });
+
+    it("get VAT", function () {
+        let val = 20;
+        expect(bill.VAT).to.be.equal(val);
+    });
+
+    it("isActive", function () {
+        let val = true;
+        expect(bill.active).to.be.equal(val);
+    });
+
+    it("is non Active", function () {
+        let val = false;
+        bill.active = false
+        expect(bill.active).to.be.equal(val);
+    });
+
+    it("set VAT", function () {
+        let val = 35;
+        bill.VAT = 35;
+        expect(bill.VAT).to.be.equal(val);
+    });
+
+    it("set wrong VAT", function () {
+        expect(() => bill.VAT = -1).to.throw(Error, 'VAT must be a non-negative number');
+    });
+
+    it("set wrong VAT, string", function () {
+        expect(() => bill.VAT = "HUI").to.throw(Error, 'VAT must be a non-negative number');
+    });
 
 
-    it("Function add", function () {
-        const str =  tst("pesho");
-        expect(str).to.be.equal('peshoasd');
+    it("set wrong name", function () {
+        expect(() => bill.name = 11).to.throw(Error, 'Name must be a non-empty string');
+    });
+
+    it("set empty name", function () {
+        expect(() => bill.name = "").to.throw(Error, 'Name must be a non-empty string');
+    });
+
+    it("set wrong value", function () {
+        expect(() => bill.value = "asd").to.throw(Error, 'Value must be a non-negative number');
+    });
+
+    it("set negative value", function () {
+        expect(() => bill.value = -12).to.throw(Error, 'Value must be a non-negative number');
+    });
+
+
+    it("is non wrong Active", function () {
+        expect(() => bill.active = 12).to.throw(Error, 'Active status must be a boolean');
+    });
+
+    it("to str, inactive", function () {
+
+        bill.active = false;
+        let str = "Package: Pesho (inactive)\n" +
+            "- Value (excl. VAT): 20\n" +
+            "- Value (VAT 20%): 24";
+        expect(bill.toString()).to.be.equal(str);
     });
 
 });
+

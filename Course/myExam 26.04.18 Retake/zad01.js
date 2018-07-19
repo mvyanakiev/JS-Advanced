90728392bc2079f7f8f70cf7f616c0b5ce309ffa
Milko Yanakiev <myanakiev@hotmail.com>
2018-07-19 14:23:33 +0300

    ua

--- Course/myExam 26.04.18 Retake/zad01.js
+++ Course/myExam 26.04.18 Retake/zad01.js
@@ -3,23 +3,22 @@ function busRoute() {
     let firstNumber = $('#enter-stops').find('input[name=first-stop]').val();
     let lastNumber = $('#enter-stops').find('input[name=last-stop]').val();
     let maxNumber = $("#bus-stops").children().length;
-let output = "";
+    let output = "";
 
 
-    let resultArray = $('#bus-stops').children().map(function(){
+    let resultArray = $('#bus-stops').children().map(function () {
         return $.trim($(this).text());
     }).get();
 
 
-
     if (firstNumber >= 1 &&
-        lastNumber < maxNumber+1 &&
-    firstNumber< lastNumber) {
+        lastNumber < maxNumber + 1 &&
+        firstNumber < lastNumber) {
 
         $('#selected-bus-stops').empty();
         $('#selected-route').empty();
 
-        for (let i = firstNumber-1; i < lastNumber; i++) {
+        for (let i = firstNumber - 1; i < lastNumber; i++) {
 
             output += `<li>${resultArray[i]}</li>`
         }
